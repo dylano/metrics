b0250c8267c8a98c6bcd822a9fe5529f55e6d90c
Dylan Oliver <d@doliver.net>
2016-05-27 21:42:36 -0700

    Update SmartHome_clickConversation.sql
    formatting

--- sql/reports/SmartHome_clickConversation.sql
+++ sql/reports/SmartHome_clickConversation.sql
@@ -1,47 +1,28 @@
-select
-  [CD-SH-DeviceID],
-
-hittimestamp, 
-
-(  case
-    when arch = '1d0038000347353138383138'
-      then 1
-    when arch = '36001e000447353138383138'
-      then 2
-    else 0
-  end )
-+
-(case
-    when eventAction = 'click'
-      then -.2
-    when eventAction = 'longpress'
-      then .2
-    else 0
-  end 
-) as plotline,
-
-case
-    when eventAction = 'click'
-      then 'Rainbow'
-    when eventAction = 'doubleclick'
-      then 'Yellow Green'
-    when eventAction = 'longpress'
-      then 'Blue'
-    else ''
-  end as ClickType
-
-
-
-
-from
-  [121095747.SH_Events]
-where
-  appname = 'smart_home'
-  and arch in (
-    '1d0038000347353138383138'
-    , '36001e000447353138383138'
-  )
-  and eventcategory in (
-    'DEVICE_1_BUTTON_PRESS'
-    , 'DEVICE_2_BUTTON_PRESS'
-  )
+SELECT [CD-SH-DeviceID], hittimestamp, ( 
+	CASE 
+		WHEN arch = '1d0038000347353138383138'  -- Device 5a
+		THEN 1 
+		WHEN arch = '36001e000447353138383138'  -- Device 5b
+		THEN 2 
+		ELSE 0 
+	END ) + ( 
+	CASE 
+		WHEN eventAction = 'click' 
+		THEN -.2
+		WHEN eventAction = 'longpress' 
+		THEN .2
+		ELSE 0 
+	END ) AS plotline, 
+	CASE 
+		WHEN eventAction = 'click' 
+		THEN 'Rainbow' 
+		WHEN eventAction = 'doubleclick' 
+		THEN 'Yellow Green' 
+		WHEN eventAction = 'longpress' 
+		THEN 'Blue' 
+		ELSE '' 
+	END AS ClickType 
+FROM [121095747.AllEvents_30Day] 
+WHERE appname = 'smart_home' 
+	AND arch IN ( '1d0038000347353138383138' , '36001e000447353138383138' )  -- Device pair 5
+	AND eventcategory IN ( 'DEVICE_1_BUTTON_PRESS' , 'DEVICE_2_BUTTON_PRESS' )
