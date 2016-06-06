cf9a28b2b8ed546049fc032a65c1c5f34459218e
Dylan Oliver <d@doliver.net>
2016-06-06 15:54:08 -0700

    Update AllEvents_7Day.sql
    Added EventTimestamp and EventValueFP columns, renamed usectime to HitTimestampUsec

--- sql/views/AllEvents_7Day.sql
+++ sql/views/AllEvents_7Day.sql
@@ -1,6 +1,7 @@
 SELECT
   STRFTIME_UTC_USEC(visitstarttime * 1000000 + hits.time*1000, "%F %T") AS HitTimestamp,
-  visitstarttime*1000000 + hits.time*1000 AS usecTime,
+  visitstarttime*1000000 + hits.time*1000 AS HitTimestampUsec,
+  max(case when hits.customdimensions.index = 7 then hits.customdimensions.value end) EventTimestamp,
   FullVisitorId,
   VisitId,
   VisitNumber,
@@ -10,6 +11,7 @@ SELECT
   hits.eventInfo.eventAction as EventAction,
   hits.eventInfo.eventLabel as EventLabel,
   hits.eventInfo.eventValue as EventValue,
+  max(case when hits.customdimensions.index = 8 then hits.customdimensions.value end) EventValueFP,
   max(case when hits.customdimensions.index = 1 then hits.customdimensions.value end) OS,
   max(case when hits.customdimensions.index = 2 then hits.customdimensions.value end) OSVersion,
   max(case when hits.customdimensions.index = 3 then hits.customdimensions.value end) Device,
@@ -17,13 +19,15 @@ SELECT
   max(case when hits.customdimensions.index = 5 then hits.customdimensions.value end) AppPlatform,
   max(case when hits.customdimensions.index = 6 then hits.customdimensions.value end) AppBuildID
 
-FROM (TABLE_DATE_RANGE([121095747.ga_sessions_], DATE_ADD(CURRENT_TIMESTAMP(), -6, 'DAY'), DATE_ADD(CURRENT_TIMESTAMP(), -1, 'DAY'))),
+FROM 
+  (TABLE_DATE_RANGE([121095747.ga_sessions_], DATE_ADD(CURRENT_TIMESTAMP(), -6, 'DAY'), DATE_ADD(CURRENT_TIMESTAMP(), -1, 'DAY'))),
   (TABLE_DATE_RANGE([121095747.ga_sessions_intraday_], DATE_ADD(CURRENT_TIMESTAMP(), -7, 'DAY'), CURRENT_TIMESTAMP()))
 
-  WHERE
+WHERE
   hits.eventInfo.eventValue is not null
 
-GROUP EACH BY hits.hitnumber, hitnumber, hitTimestamp, usectime, visitid, visitnumber, appName, eventCategory, eventAction, eventLabel, eventValue, fullVisitorId
+GROUP EACH BY 
+  hits.hitnumber, hitnumber, hitTimestamp, hitTimestampUsec, visitid, visitnumber, appName, eventCategory, eventAction, eventLabel, eventValue, fullVisitorId
 
-ORDER BY
-  1
+ORDER BY 
+  hitTimestampUsec
